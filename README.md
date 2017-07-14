95db63ab17fc4eff65d70c927de28ec8b238e64c
igulu <igulu1998@outlook.com>
2017-07-15 00:17:11 +0800

    add main file and update readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Lunan Laundry Spider
+
+### Description
+
+This project is designed to monitor the devices' occupancy of the laundry rooms which are under the administration of **Lunan Information and Technology Co., Ltd**(伦安信息科技有限公司). The current version only supports the monitoring of Student Building No.6 of Tongji University, Jiading campus.
+
+### Run
+
+```
+$ python main.py
+```
+
+### Function
+
+- [x] Obtain devices' serial numbers and occupancy
+- [ ] Obtain estimated time of finishing current task
+- [x] Send notification when finding a unoccupied device
+- [ ] Support choosing multiple laundry rooms
+- [ ] Support choosing different kind of devices
+
+### Update Log
+
+> Version 1.0 		__2017.7.14__
+
+​	* Initial Commit
---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import pync
+
+page = requests.get('http://www.iskyct.com/www/Uwash/shopsearch/shopdetail.jsp?SHOP_CD=617')
+
+soup = BeautifulSoup(page.text, 'html.parser')
+
+machines = soup('dl')
+
+while (True):
+    free = False;
+
+    for machine in machines[-3:]:
+        number = machine('b')[0].string
+        status = machine.attrs['class'][0]
+
+        print(number + "Machine", status)
+        if status == 'free':
+            free = True
+            print('Lucky for you! Go get the damn machine!')
+
+    print('Last checked time:', end=' ')
+    print(time.strftime("%H:%M:%S", time.localtime()))
+    if free:
+        pync.Notifier.notify('GOOOOOOOOO!')
+        print('\a')
+        exit(0)
+
+time.sleep(15)
