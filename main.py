1def756c0e77621afe8b8562e2027e46e9d195c8
igulu <igulu1998@outlook.com>
2017-10-23 15:29:37 +0800

    remove loop

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import json
-from pync import Notifier
 
 building = input('Please input the building number:')
 
@@ -10,35 +9,31 @@ with open('config.json', encoding='utf-8') as chart:
     load = json.load(chart)
     buildingNo = load[building]
 
-while (True):
+page = requests.get('http://www.iskyct.com/www/Uwash/shopsearch/shopdetail.jsp?SHOP_CD={}'.format(str(buildingNo)))
 
-    page = requests.get('http://www.iskyct.com/www/Uwash/shopsearch/shopdetail.jsp?SHOP_CD={}'.format(str(buildingNo)))
+soup = BeautifulSoup(page.text, 'html.parser')
 
-    soup = BeautifulSoup(page.text, 'html.parser')
+machines = soup('div', 'index_list2_con')
 
-    machines = soup('div', 'index_list2_con')
+for machineType in machines:
 
-    for machineType in machines:
+    print(machineType('h2')[0].string)
 
-        print(machineType('h2')[0].string)
+    for machine in machineType('dl'):
+        number = machine('b')[0].string
+        status = machine.attrs['class'][0]
 
-        for machine in machineType('dl'):
-            number = machine('b')[0].string
-            status = machine.attrs['class'][0]
+        print(number + "Machine", status)
+        if status == 'free':
+            free = True
+            print('Lucky for you!')
+        elif status == 'offline':
+            pass
+        else:
+            ETF = machine.span.get_text().split('\n')[1]
+            print('Estimated time of finish:', ETF)
 
-            print(number + "Machine", status)
-            if status == 'free':
-                free = True
-                print('Lucky for you!')
-                Notifier.notify('Let\'s fucking go!', title='igulu')
-            elif status == 'offline':
-                pass
-            else:
-                ETF = machine.span.get_text().split('\n')[1]
-                print('Estimated time of finish:', ETF)
+    print()
 
-        print()
-
-    print('Last checked time:', end=' ')
-    print(time.strftime("%H:%M:%S", time.localtime()))
-    time.sleep(15)
+print('Last checked time:', end=' ')
+print(time.strftime("%H:%M:%S", time.localtime()))
